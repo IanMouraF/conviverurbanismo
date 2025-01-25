7a23467d01936fc73bc838219d764956b7e86243
IanMouraF <ianmourafac@gmail.com>
2025-01-25 07:37:01 -0300

    novo swapper hidden

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,23 @@ var swiper = new Swiper(".swiper", {
     delay: 5000,
   },
 });
+
+var swiper1 = new Swiper(".gallery-thumbs", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".gallery-top", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,9 +1,39 @@
-// Smooth Scrolling
-document.querySelectorAll('a[href^="#"').forEach(anchor => {
-  anchor.addEventListener('click',
-    function (e) {
-      e.preventDefault()
-      document.querySelector(this.getAttribute('href')).scrollIntoView({behavior:'smooth'})
-    }
-  )
-})
+// Esconder o swiper
+
+const galleryTop = new Swiper(".gallery-top", {
+  spaceBetween: 10,
+  navigation: true,
+});
+
+const galleryThumbs = new Swiper(".gallery-thumbs", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  centeredSlides: true,
+  slideToClickedSlide: true,
+});
+
+galleryTop.controller.control = galleryThumbs;
+galleryThumbs.controller.control = galleryTop;
+
+// Mostrar o Swiper em tela cheia ao clicar na imagem
+const galleryImages = document.querySelectorAll(".image-container");
+const fullscreenSwiper = document.getElementById("fullscreen-swiper");
+const closeButton = document.querySelector(".swiper-button-close");
+
+closeButton.addEventListener("click", () => {
+  console.log("Botão de fechar clicado!");
+  fullscreenSwiper.classList.add("hidden");
+});
+
+
+galleryImages.forEach((image, index) => {
+  image.addEventListener("click", () => {
+    fullscreenSwiper.classList.remove("hidden");
+    galleryTop.slideTo(index);
+  });
+});
+
+// Fechar o Swiper em tela cheia
+closeButton.addEventListener("click", () => {
+  fullscreenSwiper.classList.add("hidden");
+});
