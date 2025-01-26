90c3a5bcb3b9c43cbe2b742538f80fbf39b857af
IanMouraF <ianmourafac@gmail.com>
2025-01-26 08:01:27 -0300

    concluidas seções grid e carrosseis

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
-var swiper = new Swiper(".swiper", {
+var swiper = new Swiper(".carrossel .swiper", {
+  speed: 700,
   loop: true,
   slidesPerView: 1,
   spaceBetween: 30,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".carrossel .swiper-button-next",
+    prevEl: ".carrossel .swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
@@ -13,26 +14,7 @@ var swiper = new Swiper(".swiper", {
   dynamicBullets: true,
   dynamicMainBullets: 1,
   autoplay: {
-    delay: 5000,
-  },
-});
-
-var swiper1 = new Swiper(".gallery-thumbs", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var swiper2 = new Swiper(".gallery-top", {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
+    delay: 8000,
   },
 });
 
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,24 +1,37 @@
 // Esconder o swiper
-
-const galleryTop = new Swiper(".gallery-top", {
-  spaceBetween: 10,
-  navigation: true,
-});
-
-const galleryThumbs = new Swiper(".gallery-thumbs", {
-  spaceBetween: 10,
+const galleryThumbs = new Swiper(".mySwiper", {
+  
+  loop: true,
+  spaceBetween: 20,
   slidesPerView: 4,
-  centeredSlides: true,
-  slideToClickedSlide: true,
+  freeMode: true,
+  watchSlidesProgress: true,
 });
 
-galleryTop.controller.control = galleryThumbs;
-galleryThumbs.controller.control = galleryTop;
+const galleryTop = new Swiper(".mySwiper2", {
+  effect: 'coverflow',
+  speed: 800,
+  spaceBetween: 10,
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: galleryThumbs,
+  },
+});
+
 
 // Mostrar o Swiper em tela cheia ao clicar na imagem
 const galleryImages = document.querySelectorAll(".image-container");
 const fullscreenSwiper = document.getElementById("fullscreen-swiper");
 const closeButton = document.querySelector(".swiper-button-close");
+const swiperContent = document.querySelector(".mySwiper2 .swiper-slide img");
+const prevArrow = document.querySelector(".swiperthumb .swiper-button-prev");
+const nextArrow = document.querySelector(".swiperthumb .swiper-button-next");
+const thumbsContainer = document.querySelector(".mySwiper");
 
 closeButton.addEventListener("click", () => {
   console.log("Botão de fechar clicado!");
@@ -28,12 +41,26 @@ closeButton.addEventListener("click", () => {
 
 galleryImages.forEach((image, index) => {
   image.addEventListener("click", () => {
+    console.log("Imagem clicada");
     fullscreenSwiper.classList.remove("hidden");
     galleryTop.slideTo(index);
+    console.log(index)
   });
 });
 
 // Fechar o Swiper em tela cheia
 closeButton.addEventListener("click", () => {
   fullscreenSwiper.classList.add("hidden");
+});
+
+// Fechar ao clicar fora da imagem
+fullscreenSwiper.addEventListener("click", (event) => {
+  if (
+    !swiperContent.contains(event.target) && // Fora da imagem principal
+    !prevArrow.contains(event.target) && // Fora da seta "Anterior"
+    !nextArrow.contains(event.target) && // Fora da seta "Próxima"
+    !thumbsContainer.contains(event.target) // Fora das miniaturas
+  ) {
+    fullscreenSwiper.classList.add("hidden");
+  }
 });
